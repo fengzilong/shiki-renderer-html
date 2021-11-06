f4622b46e0b6c3947e6fa3d768529e2aca1491df
MO <fengzilong1992@gmail.com>
2021-11-06 12:01:46 +0800

    refactor: add more types

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,47 @@
+/* eslint-disable max-len */
 // Modified from
 // https://github.com/shikijs/shiki/blob/main/packages/shiki/src/renderer.ts
 
 import { FontStyle } from 'shiki'
 import type { IThemedToken } from 'shiki'
 
-export interface ElementProps {
-  children: string
-  [key: string]: unknown
+export type ElementType = 'pre' | 'code' | 'line' | 'token'
+export interface PreElementProps {
+  className: string,
+  style: string,
+  children: string,
 }
 
-export interface TokenElementProps extends ElementProps {
+export interface CodeElementProps {
+  children: string
+}
+
+export interface LineElementProps {
+  className: string,
+  lines: IThemedToken[][],
+  line: IThemedToken[],
+  index: number,
+  children: string,
+}
+
+export interface TokenElementProps {
   style: string
   tokens: IThemedToken[]
   token: IThemedToken
-  index: number
+  index: number,
+  children: string,
 }
 
-export interface ElementsOptions {
-  [key: string]: ( props: ElementProps ) => string
+export type PreElementFunction = ( props: PreElementProps ) => string
+export type CodeElementFunction = ( props: CodeElementProps ) => string
+export type LineElementFunction = ( props: LineElementProps ) => string
+export type TokenElementFunction = ( props: TokenElementProps ) => string
+
+export type ElementsOptions = {
+  pre: PreElementFunction,
+  code: CodeElementFunction,
+  line: LineElementFunction,
+  token: TokenElementFunction,
 }
 
 export interface HtmlRendererOptions {
@@ -52,22 +76,50 @@ const defaultElements: ElementsOptions = {
 export function render(
   lines: IThemedToken[][],
   options: HtmlRendererOptions = {}
-) {
+): string {
   const bg = options.bg || '#fff'
-  const userElements = options.elements || {}
+  const userElements: ElementsOptions | undefined = options.elements
 
   function h(
-    type: string,
-    props: object,
+    type: ElementType,
+    props: { className?: string; style?: string; lines?: IThemedToken[][]; line?: IThemedToken[]; index?: number; tokens?: IThemedToken[]; token?: IThemedToken },
     children: string[]
   ): string {
-    const element = userElements[ type ] || defaultElements[ type ]
-    if ( element ) {
-      children = children.filter( Boolean )
+    const element = ( userElements && userElements[ type ] ) ||
+      defaultElements[ type ]
+    children = children.filter( Boolean )
 
-      return element( {
-        ...props,
-        children: type === 'code' ? children.join( '\n' ) : children.join( '' )
+    if ( type === 'pre' ) {
+      return ( element as PreElementFunction )( {
+        className: props.className || '',
+        style: props.style || '',
+        children: children.join( '' )
+      } )
+    }
+
+    if ( type === 'code' ) {
+      return ( element as CodeElementFunction )( {
+        children: children.join( '\n' )
+      } )
+    }
+
+    if ( type === 'line' ) {
+      return ( element as LineElementFunction )( {
+        className: props.className || '',
+        lines: props.lines || [],
+        line: props.line as IThemedToken[],
+        index: props.index as number,
+        children: children.join( '' )
+      } )
+    }
+
+    if ( type === 'token' ) {
+      return ( element as TokenElementFunction )( {
+        style: props.style || '',
+        tokens: props.tokens || [],
+        token: props.token as IThemedToken,
+        index: props.index as number,
+        children: children.join( '' )
       } )
     }
 
